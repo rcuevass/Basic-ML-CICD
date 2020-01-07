cd9034a29745a8df5a6451c6a1a92bcdd1e5ff17
rcuevass <rogercuevas.ca@gmail.com>
2020-01-07 13:43:24 -0500

    updating setup.py

--- fashion_mnist_classifier/setup.py
+++ fashion_mnist_classifier/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mnist-fashion-classifier',
+    name='fashion_mnist_classifier',
     version="0.1.0",
     packages=find_packages(),
 )
